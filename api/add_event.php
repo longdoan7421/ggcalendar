123f215b64cbe8cf59757af54aa80f129bd00182
Doan Vu Hoang Long <longdoan@mmj.vn>
2019-05-21 09:02:57 +0700

    try different way to authorizng requests with google

--- api/add_event.php
+++ api/add_event.php
@@ -1,55 +1,68 @@
 <?php
-  require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-  session_start();
+session_start();
 
-  $dotenv = Dotenv\Dotenv::create(__DIR__, '/../.env');
-  $dotenv->load();
-  
-  $inputJSON = file_get_contents('php://input');
-  if ($inputJSON) {
-    $appointments = json_decode($inputJSON, true);
+$dotenv = Dotenv\Dotenv::create(__DIR__, '/../.env');
+$dotenv->load();
 
-    $newAppointment = [];
-    foreach ($appointments as $appointment) {
-      $newAppointment = array(
-        'summary' => $appointment['Title'],
-        'location' => isset($appointment['Location']) ? $appointment['Location'] : '',
-        'description' => isset($appointment['Description']) ? $appointment['Description'] : '' ,
-        'start' => array(
-          'dateTime' => date('c', strtotime($appointment['StartTime']))
-        ),
-        'end' => array(
-          'dateTime' => date('c', strtotime($appointment['EndTime']))
-        )
-      );
-    }
+$inputJSON = file_get_contents('php://input');
+if ($inputJSON) {
+  $appointments = json_decode($inputJSON, true);
 
-    $client = new Google_Client();
-    if (isset($_SESSION['token'])) {
-      $client->setAccessToken($_SESSION['token']);
-     }
-    $client->setScopes(Google_Service_Calendar::CALENDAR);
-    $client->useApplicationDefaultCredentials();
-
-    $service = new Google_Service_Calendar($client);
-    $event = new Google_Service_Calendar_Event($newAppointment);
-    $calendarId = getenv('CALENDAR_ID');
-
-    try {
-      $result = $service->events->insert($calendarId, $event);
-      echo json_encode([
-        'code' => 200,
-        'event' => [
-          'id' => $result->id,
-          'link' => $result->htmlLink
-        ]
-      ]);
-    } catch (Google_Service_Exception $error) {
-      echo json_encode([
-        'code' => 500,
-        'errors' => $error->getErrors()
-      ]);
-    }
+  $newAppointment = [];
+  foreach ($appointments as $appointment) {
+    $newAppointment = array(
+      'summary' => $appointment['Title'],
+      'location' => isset($appointment['Location']) ? $appointment['Location'] : '',
+      'description' => isset($appointment['Description']) ? $appointment['Description'] : '',
+      'start' => array(
+        'dateTime' => date('c', strtotime($appointment['StartTime']))
+      ),
+      'end' => array(
+        'dateTime' => date('c', strtotime($appointment['EndTime']))
+      )
+    );
   }
-?>
+
+  if (getenv('NODE_ENV') === 'production') {
+    $private_key = base64_decode(getenv('GOOGLE_API_PRIVATE_KEY_BASE64'));
+    $private_key = str_replace('\n', "\n", $private_key);
+    $client_params = [
+      'client_email'        => getenv('GOOGLE_API_CLIENT_EMAIL'),
+      'signing_algorithm'   => 'HS256',
+      'signing_key'         => $private_key
+    ];
+    $client = new Google_Client($client_params);
+    $client->setClientId(getenv('GOOGLE_API_CLIENT_ID'));
+  } else {
+    $client = new Google_Client();
+  }
+
+  $client->useApplicationDefaultCredentials();
+
+  if (isset($_SESSION['token'])) {
+    $client->setAccessToken($_SESSION['token']);
+  }
+  $client->setScopes(Google_Service_Calendar::CALENDAR);
+
+  $service = new Google_Service_Calendar($client);
+  $event = new Google_Service_Calendar_Event($newAppointment);
+  $calendarId = getenv('CALENDAR_ID');
+
+  try {
+    $result = $service->events->insert($calendarId, $event);
+    echo json_encode([
+      'code' => 200,
+      'event' => [
+        'id' => $result->id,
+        'link' => $result->htmlLink
+      ]
+    ]);
+  } catch (Google_Service_Exception $error) {
+    echo json_encode([
+      'code' => 500,
+      'errors' => $error->getErrors()
+    ]);
+  }
+}
