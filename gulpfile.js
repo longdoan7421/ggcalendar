00527863f5a9c02e29828b988147c51e64d8f2de
Doan Vu Hoang Long <longdoan@mmj.vn>
2019-07-09 17:04:53 +0700

    restructuring folders

--- gulpfile.js
+++ gulpfile.js
@@ -2,44 +2,47 @@
 
 var gulp = require('gulp');
 
-gulp.task('serve', ['build'], function (done) {
-    var browserSync = require('browser-sync');
-    var bs = browserSync.create('GGCalendar');
-    var options = {
-        server: {
-            baseDir: ['./dist/']
+gulp.task('serve', ['build'], function(done) {
+  var browserSync = require('browser-sync');
+  var bs = browserSync.create('GGCalendar');
+  var options = {
+    server: {
+      baseDir: ['./dist/']
+    },
+    ui: false
+  };
+  bs.init(options, done);
+  gulp.watch('src/**/*.ts', ['compile', bs.reload]);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.ts', ['compile']);
+});
+
+gulp.task('compile', function(done) {
+  var webpack = require('webpack');
+  var webpackStream = require('webpack-stream');
+  gulp.src(['./src/index.ts'])
+    .pipe(
+      webpackStream(
+        {
+          config: require('./webpack.config.js')
         },
-        ui: false
-    };
-    bs.init(options, done);
-    gulp.watch('src/**/*.ts', ['compile', bs.reload]);
+        webpack
+      )
+    )
+    .pipe(gulp.dest('./dist/js'))
+    .on('end', function() {
+      done();
+    });
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/**/*.ts', ['compile']);
+gulp.task('css', function() {
+  gulp.src('./node_modules/@syncfusion/ej2/material.css').pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('compile', function (done) {
-    var webpack = require('webpack');
-    var webpackStream = require('webpack-stream');
-    gulp
-        .src(['./src/index.ts'])
-        .pipe(
-            webpackStream(
-                {
-                    config: require('./webpack.config.js')
-                },
-                webpack
-            )
-        )
-        .pipe(gulp.dest('./dist/js'))
-        .on('end', function () {
-            done();
-        });
+gulp.task('resources', function() {
+  gulp.src('./resources/**/*').pipe(gulp.dest('./dist'));
 });
 
-gulp.task('css', function () {
-    gulp.src('./node_modules/@syncfusion/ej2/material.css').pipe(gulp.dest('./dist/css'));
-});
-
-gulp.task('build', ['compile', 'css']);
+gulp.task('build', ['compile', 'css', 'resources']);
